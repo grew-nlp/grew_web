e3e4238026df1cc517591b5b8154c4744d4745a5
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2020-10-11 21:41:44 +0200

    cleaning

--- index.js
+++ index.js
@@ -1,48 +1,19 @@
-
-
-// // update of the file selector content
-// $(".custom-file-input").on("change", function() {
-//   var fileName = $(this).val().split("\\").pop();
-//   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-// });
-
-
-
-
 var current = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
     corpus: 'No corpus loaded',
-    sent_ids: ['ABJ_GWA_08_David-Lifestory_MG__1', 'ABJ_GWA_08_David-Lifestory_MG__134']
+    sent_ids: []
   },
-
   methods: {
-    greet: function (event) {
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this.id + '!')
-      // `event` is the native DOM event
-      if (event) {
-        alert(event.target.tagName)
-      }
-    },
-    toto: function (event) {
-      console.log("+++++++++++++++++++");
-      console.log(event.target.id);
-      console.log("+++++++++++++++++++");
+    select_graph: function(event) {
+      console.log("[select_graph] " + event.target.id);
     }
-
   }
 })
 
-// binding on sent_id items
-$(".sent").on('click', function() {
-  alert(this.id);
-})
 
 
-
-const uploadFile = file => {
+function uploadFile(file) {
   console.log("Uploading file...");
   const API_ENDPOINT = "http://localhost:8080/upload";
   const request = new XMLHttpRequest();
@@ -50,28 +21,29 @@ const uploadFile = file => {
 
   request.open("POST", API_ENDPOINT, true);
   request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      resp = JSON.parse (request.responseText);
-      console.log(resp.data.sent_ids);
-      $("#reply").html (request.responseText);
-       current.message = "John Doe";
-       current.sent_ids = resp.data.sent_ids;
-       // add bindings
-       $(".sent").on('click', function() {
-         alert(this.id);
-       })
-
-    }
-    else {
-      $("#reply").html ("ERROR");
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        resp = JSON.parse(request.responseText);
+        if (resp.status === "ERROR") {
+          alert("[ERROR, file " + file.name + "] " + resp.message.message);
+        } else {
+          console.log(request.responseText);
+          current.sent_ids = resp.data.sent_ids;
+        }
+      } else {
+        console.log(file);
+        alert("[ERROR] cannot load: " + file.name);
+      }
+    } else {
+      console.log("still waiting");
     }
   };
   formData.append("file", file);
   request.send(formData);
-};
+}
 
-$("#fileInput").change (function (event)
-{
+// Binding on fileInput
+$("#fileInput").change(function(event) {
   const files = event.target.files;
   uploadFile(files[0]);
 })
