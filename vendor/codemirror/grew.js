73c3f7bac50f3eb27552ac2fa280d2801d8ad679
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2021-02-27 09:33:37 +0100

    Review codemirror grew mode

--- vendor/codemirror/grew.js
+++ vendor/codemirror/grew.js
@@ -2,99 +2,116 @@
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 (function(mod) {
-    if (typeof exports == "object" && typeof module == "object") // CommonJS
-	mod(require("../../lib/codemirror"));
-    else if (typeof define == "function" && define.amd) // AMD
-	define(["../../lib/codemirror"], mod);
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
   else // Plain browser env
-      mod(CodeMirror);
+    mod(CodeMirror);
 })(function(CodeMirror) {
-    "use strict";
+  "use strict";
 
-    CodeMirror.defineMode('mllike', function(_config, parserConfig) {
-	var words = {
-	    'global': 'builtin',
-	    'pattern': 'builtin',
-	    'without': 'builtin',
-	};
+  CodeMirror.defineMode('mllike', function(_config, parserConfig) {
+    var words = {
+      'global': 'builtin',
+      'pattern': 'builtin',
+      'without': 'builtin',
+      'commands': 'builtin',
+      'rule': 'keyword',
+      'package': 'keyword',
+      'strat': 'keyword',
+      'Iter': 'special',
+      'Seq': 'special',
+      'Onf': 'special',
+      'Alt': 'special',
+      'Pick': 'special',
+      'Try': 'special',
+      'If': 'special',
+    };
 
-	function tokenBase(stream, state) {
-	    var ch = stream.next();
+    function tokenBase(stream, state) {
+      var ch = stream.next();
 
-	    if (ch === '"') {
-		state.tokenize = tokenString;
-		return state.tokenize(stream, state);
-	    }
+      if (ch === '"') {
+        state.tokenize = tokenString;
+        return state.tokenize(stream, state);
+      }
 
-	    if (ch === '%' ) {
-		stream.skipToEnd();
-		return 'comment';
-	    }
+      if (ch === '%') {
+        stream.skipToEnd();
+        return 'comment';
+      }
 
-	    if (ch === '=' ) {
-                return 'quote';
-            }
+      if (ch === '=') {
+        return 'quote';
+      }
 
-            if (ch === '<') {
-		if (stream.eat('>')) {
-                    return 'quote';
-                }
-            }
+      if (ch === '<') {
+        if (stream.eat('>')) {
+          return 'quote';
+        }
+      }
 
-	    if (ch === '-') {
-	    	var next_ch = stream.next();
-	    	if ((next_ch === '[') || (next_ch === ">")) {
-		    return 'quote';
-		}
-	    }
+      if (ch === '-') {
+        var next_ch = stream.next();
+        if ((next_ch === '[') || (next_ch === ">")) {
+          return 'quote';
+        }
+      }
 
-            if (ch === ']') {
-		if (stream.eat('-') && stream.eat(">")) {
-                    return 'quote';
-                }
-            }
+      if (ch === ']') {
+        if (stream.eat('-') && stream.eat(">")) {
+          return 'quote';
+        }
+      }
 
 
 
-	    if (/\d/.test(ch)) {
-		stream.eatWhile(/[\d]/);
-		if (stream.eat('.')) {
-		    stream.eatWhile(/[\d]/);
-		}
-		return 'number';
-	    }
-	    if ( /[+\-*&%=<>!?|]/.test(ch)) {
-		return 'operator';
-	    }
-	    stream.eatWhile(/\w/);
-	    var cur = stream.current();
-	    return words[cur] || 'variable';
-	}
+      if (/\d/.test(ch)) {
+        stream.eatWhile(/[\d]/);
+        if (stream.eat('.')) {
+          stream.eatWhile(/[\d]/);
+        }
+        return 'number';
+      }
+      if (/[+\-*&%=<>!?|]/.test(ch)) {
+        return 'operator';
+      }
+      stream.eatWhile(/\w/);
+      var cur = stream.current();
+      return words[cur] || 'variable';
+    }
 
-	function tokenString(stream, state) {
-	    var next, end = false, escaped = false;
-	    while ((next = stream.next()) != null) {
-		if (next === '"' && !escaped) {
-		    end = true;
-		    break;
-		}
-		escaped = !escaped && next === '\\';
-	    }
-	    if (end && !escaped) {
-		state.tokenize = tokenBase;
-	    }
-	    return 'string';
-	}
+    function tokenString(stream, state) {
+      var next, end = false,
+        escaped = false;
+      while ((next = stream.next()) != null) {
+        if (next === '"' && !escaped) {
+          end = true;
+          break;
+        }
+        escaped = !escaped && next === '\\';
+      }
+      if (end && !escaped) {
+        state.tokenize = tokenBase;
+      }
+      return 'string';
+    }
 
-	return {
-	    startState: function() {return {tokenize: tokenBase, commentLevel: 0};},
-	    token: function(stream, state) {
-		if (stream.eatSpace()) return null;
-		return state.tokenize(stream, state);
-	    },
-	    lineComment: "%"
-	};
-    });
+    return {
+      startState: function() {
+        return {
+          tokenize: tokenBase,
+          commentLevel: 0
+        };
+      },
+      token: function(stream, state) {
+        if (stream.eatSpace()) return null;
+        return state.tokenize(stream, state);
+      },
+      lineComment: "%"
+    };
+  });
 
-    CodeMirror.defineMIME("text/grew", "grew");
+  CodeMirror.defineMIME("text/grew", "grew");
 });
